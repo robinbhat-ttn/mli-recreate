922c612bb3785b8c3d6dd34b96c1ac84238ff2a6
punsay <punsay.prakhar@tothenew.com>
2025-12-18 00:20:26 +0530

    icons addded in header - desktop

--- src/components/features/ctf-components/ctf-header/NavigationContainer.tsx
+++ src/components/features/ctf-components/ctf-header/NavigationContainer.tsx
@@ -1,5 +1,6 @@
 import { useContentfulInspectorMode } from '@contentful/live-preview/react';
 import { Box } from '@mui/material';
+import Image from 'next/image';
 import Link from 'next/link';
 
 import styles from './ctf-header.module.scss';
@@ -7,6 +8,7 @@ import type { NavigationFieldsFragment } from '../ctf-navigation/__generated/ctf
 
 export const NavigationContainer = (props: NavigationFieldsFragment) => {
   const inspectorMode = useContentfulInspectorMode();
+  console.log('NavigationContainer props:', props);
   return (
     <Box
       component="nav"
@@ -49,7 +51,20 @@ export const NavigationContainer = (props: NavigationFieldsFragment) => {
                       href={subItem?.mainLink?.slug ?? subItem?.mainLink?.linkUrl ?? '/'}
                       {...inspectorMode({ entryId: subItem?.sys.id, fieldId: 'link' })}
                     >
-                      {subItem?.mainLink?.linkHeading}
+                      {subItem?.mainLink?.icon?.url && (
+                        <Image
+                          src={subItem.mainLink.icon.url}
+                          alt={
+                            subItem.mainLink.icon.title ??
+                            subItem.mainLink.linkHeading ??
+                            'Navigation icon'
+                          }
+                          width={subItem.mainLink.icon.width || 24}
+                          height={subItem.mainLink.icon.height || 24}
+                          className={styles.subNavigationItemIcon}
+                        />
+                      )}
+                      <span>{subItem?.mainLink?.linkHeading}</span>
                     </Link>
                   </div>
                   <>
