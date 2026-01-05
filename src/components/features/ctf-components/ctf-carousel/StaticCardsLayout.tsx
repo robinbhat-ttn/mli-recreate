34eceb0659a15baaf6e9ec9f436a9f5b86d55fa0
Lovepreet Singh <lovepreet.singh1@tothenew.com>
2026-01-05 17:04:46 +0530

    Card Layout UI + Backend

---
+++ src/components/features/ctf-components/ctf-carousel/StaticCardsLayout.tsx
@@ -0,0 +1,58 @@
+import Image from 'next/image';
+import { CarouselFieldsFragment } from './__generated/ctf-carousel.generated';
+import styles from './StaticCardsLayout.module.scss';
+import { CtfRichtext } from '../ctf-richtext/ctf-richtext';
+
+export const StaticCardsLayout = (props: CarouselFieldsFragment) => {
+  const cardSectionTitle = props?.carouselTitle;
+  const cards = props?.cardsCollection?.items || [];
+  return (
+    <div className={styles.staticCardsWrapper}>
+      <div className={styles.staticCardsContainer}>
+        <p className={styles['staticCardsContainer__card-section-title']}>{cardSectionTitle}</p>
+        <div className={styles['staticCardsContainer__card-container']}>
+          {cards.map(card => {
+            const id = card?.sys?.id ?? Math.random().toString(36).slice(2, 9);
+            const img = card?.cardImage;
+            const title = card?.cardTitle;
+            const content = card?.cardContent;
+            return (
+              <div
+                key={id}
+                className={styles['staticCardsContainer__card-container__card-wrapper']}
+              >
+                {img && (
+                  <Image
+                    priority
+                    fetchPriority="high"
+                    src={img?.url ?? ''}
+                    alt={img?.title ?? ''}
+                    height={43}
+                    width={43}
+                    className={styles['staticCardsContainer__card-container__card-wrapper__image']}
+                  />
+                )}
+                {title && (
+                  <div
+                    className={styles['staticCardsContainer__card-container__card-wrapper__title']}
+                  >
+                    {title}
+                  </div>
+                )}
+                {content && (
+                  <div
+                    className={
+                      styles['staticCardsContainer__card-container__card-wrapper__content']
+                    }
+                  >
+                    <CtfRichtext {...content} disableContainer />
+                  </div>
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
